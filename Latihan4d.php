42d8364144439a398040b33595a7af251b83c36f
Wilu Tama Angraini <114303279+wilutamaangraini@users.noreply.github.com>
2022-09-26 13:02:53 +0700

    Update Latihan4d.php

--- Latihan4d.php
+++ Latihan4d.php
@@ -1,33 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Array Multidimensi </title>
-</head>
-<body>
-<table border="1">
-<tr><td>Negara</td><td>Ibukota</td><td>Kode Telpon</td></tr>
 <?php
-$Negara = array(
-		array("Indonesia", "D.K.I Jakarta", "+62"),
-		array("Singapura", "Singapura", "+65"),
-		array("Malaysia", "Kuala Lumpur", "+60"),
-		array("Brunei", "Bandar Seri Begawan", "+673"),
-		array("Thailand", "Bangkok", "+66"),
-        array("Laos", "Vientiane", "+856"),
-        array("Filipina", "manila", "+63"),
-        array("Myanmar", "Naypyidaw", "+95"),
-		);
-			
-    
-        foreach ($Negara as $each){
-        
-            echo "<tr>";
-            foreach ($each as $ibukota){
-                echo "<td>" . $ibukota . "</td>";
-            }
-            echo "</tr>";
-}
+
+$data = [
+array("Indonesia","D.K.I Jakarta", "+62"),
+array("Singapura","Singapura", "+65"),
+array("Malaysia","Kuala Lumpur", "+60"),
+array("Brunei","Bandar Seri Begawan", "+673"),
+array("Thailand","Bangkok", "+66"),
+array("Laos","Vientiane", "+856"),
+array("Filipina","Manila", "+63"),
+array("Myanmar","Naypyidaw", "+95"),
+];
+ 
+echo "<table border='1'";
+echo "<tr>
+      <th>Negara</th>
+      <th>Ibukota</th>
+      <th>Kode Telepon</th>
+      </tr>";
+      
+foreach ($data as $rows => $row)
+{
+	echo "<tr>";
+	foreach ($row as $col => $cell)
+	{
+		echo "<td>" . $cell . "</td>";
+	}	
+ 
+}	
+  echo "</tr></table>";
 ?>
-</table>
-</body>
-</html>
